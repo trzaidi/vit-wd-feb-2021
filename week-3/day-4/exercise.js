5364039b3c10e6d867402dfb9d52cda47407b6ff
Taimoor Zaidi <trzaidi@gmail.com>
2021-03-18 12:47:38 -0700

    Project Push

--- week-3/day-4/exercise.js
+++ week-3/day-4/exercise.js
@@ -1,6 +1,20 @@
-function calculateTip() {
-  var billAmountValue = document.getElementById("billAmount").nodeValue;
+function calculateTip10() {
+  var billAmountValue = document.getElementById("billAmount").value;
+  var tipAmount = billAmountValue * 0.10;
+  var outputElement = document.getElementById("output");
+  outputElement.innerText = tipAmount;
+}
+
+function calculateTip15() {
+  var billAmountValue = document.getElementById("billAmount").value;
   var tipAmount = billAmountValue * 0.15;
+  var outputElement = document.getElementById("output");
+  outputElement.innerText = tipAmount;
+}
+
+function calculateTip20() {
+  var billAmountValue = document.getElementById("billAmount").value;
+  var tipAmount = billAmountValue * 0.20;
   var outputElement = document.getElementById("output");
   outputElement.innerText = tipAmount;
 }
